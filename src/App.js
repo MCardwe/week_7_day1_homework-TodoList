7fc054d3cf833a24fe2e15316f9f77e6dac862d5
Max Cardwell <maximillian.cardwell@gmail.com>
2022-04-04 14:56:35 +0100

    Finished MVP and extention

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {name: 'Buy Shopping', priority: 'low'}, {name: 'Clean bathroom', priority: 'high'},{name: "Car's MOT", priority: 'low'}
+  ]);
+
+  const [newTask, setNewTask] = useState ('');
+  const [taskPriority, setPriority] = useState('');
+
+  const handleNewTask = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const handlePriority = (event) => {
+    setPriority(event.target.value);
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks];
+    copyTasks.push({name : newTask, priority: taskPriority});
+    setTasks(copyTasks);
+    setNewTask('')
+  }
+
+  const taskNodes = tasks.map((task, index) => {
+    return (
+      <li key={index} className={task.priority == 'high' ? 'high-priority': 'low-priority'}>
+        <span>{task.name}</span>
+      </li>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>To Do's</h1>
+      <form onSubmit = {saveNewTask}>
+        <label htmlfor='new-task'>New Task: </label>
+        <input type='text' value={newTask} onChange={handleNewTask}/>
+
+        <label> <input name='priority' type='radio' value='high' onChange={handlePriority}></input>High</label>
+
+        <label> <input name='priority' type='radio' value='low' onChange={handlePriority}></input>Low</label>
+        <button type='submit' value ='Save Task'></button>
+      </form>
+
+      <ul>
+        {taskNodes}
+      </ul>
+    </>
   );
 }
 
